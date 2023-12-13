6190efd2e7a3c038fc8841b60722eda6537048da
maxdegers <maxdegers@gmail.com>
2023-12-13 15:00:10 +0100

    added

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,15 @@
 /*   By: mbrousse <mbrousse@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 10:35:48 by mbrousse          #+#    #+#             */
-/*   Updated: 2023/12/11 21:50:30 by mbrousse         ###   ########.fr       */
+/*   Updated: 2023/12/13 14:18:44 by mbrousse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
+#include <stdarg.h>
 
-int	ft_printf(const char *, ...)
+int	ft_printf(const char *str, ...)
 {
-	
+	va_list	ap;
+	va_arg();
 }
